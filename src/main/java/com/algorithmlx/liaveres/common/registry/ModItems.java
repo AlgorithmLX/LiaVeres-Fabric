97800c2e7df4d891493ac72a74e9de2bb834634f
AlgorithmLX <46783751+IgroGames2227@users.noreply.github.com>
2021-07-23 00:43:44 +0700

    Update ModItems.java

--- src/main/java/com/algorithmlx/liaveres/common/registry/ModItems.java
+++ src/main/java/com/algorithmlx/liaveres/common/registry/ModItems.java
@@ -2,7 +2,7 @@ package com.algorithmlx.liaveres.common.registry;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
-import static com.algorithmlx.liaveres.common.registry.ModItems.registerItem; // импортировали только метод
+import static com.algorithmlx.liaveres.common.registry.ModItems.registerItem;
 
 public class ModItems {
     public static final Item MatterCrystal = registerItem("ruby", new RubyItem(new FabricItemSettings()));

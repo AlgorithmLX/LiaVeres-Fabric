565ede55e89c605c06df0e6f090188b21fd3775f
IgroGames2227 <izhukov2007@gmail.com>
2021-07-22 20:50:52 +0700

    Upd

--- src/main/java/com/algorithmlx/liaveres/LiaVeres.java
+++ src/main/java/com/algorithmlx/liaveres/LiaVeres.java
@@ -3,11 +3,11 @@ import net.fabricmc.api.ModInitializer;
 import software.bernie.geckolib3.GeckoLib;
 
 public class LiaVeres implements ModInitializer {
-    GeckoLib.onInitialize()
 
     public static final String ModId = "lv";
     @Override
     public void onInitialize() {
         ModItems.init();
+        GeckoLib;
     }
 }

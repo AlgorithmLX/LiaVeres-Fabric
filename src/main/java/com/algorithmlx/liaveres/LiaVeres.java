50a82faafc6bf3e9ffd333650e28ea60022cf702
IgroGames2227 <izhukov2007@gmail.com>
2021-08-03 16:55:21 +0700

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/main/java/com/algorithmlx/liaveres/LiaVeres.java
+++ src/main/java/com/algorithmlx/liaveres/LiaVeres.java
@@ -3,6 +3,10 @@ package com.algorithmlx.liaveres;
 import com.algorithmlx.liaveres.common.registry.ModBlocks;
 import com.algorithmlx.liaveres.common.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 
 public class LiaVeres implements ModInitializer {
 
@@ -12,4 +16,6 @@ public class LiaVeres implements ModInitializer {
         ModItems.init();
         ModBlocks.init();
     }
+    public static final ItemGroup LiaVeres_GroupItems = FabricItemGroupBuilder.create(new Identifier(ModId, "liaveres_groupitems")).icon(() -> new ItemStack(ModItems.MatterCrystal)).build();
+    public static final ItemGroup LiaVeres_GroupBlocks = FabricItemGroupBuilder.create(new Identifier(ModId, "liaveres_groupblocks")).icon(() -> new ItemStack(ModBlocks.MatterCrystalBlock)).build();
 }

71de371d96dc4a230977db64634c3517449e9498
IgroGames2227 <izhukov2007@gmail.com>
2021-08-04 17:57:41 +0700

    .

--- src/main/java/com/algorithmlx/liaveres/LiaVeres.java
+++ src/main/java/com/algorithmlx/liaveres/LiaVeres.java
@@ -16,6 +16,10 @@ public class LiaVeres implements ModInitializer {
         ModItems.registerItems();
         ModBlocks.registerBlocks();
     }
-    public static final ItemGroup LiaVeres_GroupItems = FabricItemGroupBuilder.create(new Identifier(ModId, "liaveres_groupitems")).icon(() -> new ItemStack(ModItems.MATTER_CRYSTAL)).build();
-    public static final ItemGroup LiaVeres_GroupBlocks = FabricItemGroupBuilder.create(new Identifier(ModId, "liaveres_groupblocks")).icon(() -> new ItemStack(ModBlocks.MATTER_CRYSTAL_BLOCK)).build();
+    public static final ItemGroup LiaVeres_GroupItems = FabricItemGroupBuilder.create(new Identifier(ModId, "liaveres_groupitems"))
+            .icon(() -> new ItemStack(ModItems.MATTER_CRYSTAL))
+            .build();
+    public static final ItemGroup LiaVeres_GroupBlocks = FabricItemGroupBuilder.create(new Identifier(ModId, "liaveres_groupblocks"))
+            .icon(() -> new ItemStack(ModBlocks.MATTER_CRYSTAL_BLOCK))
+            .build();
 }

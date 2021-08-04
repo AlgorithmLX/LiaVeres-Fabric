8d1e1069ba7dab9c7d1c95bde877c1845918b1b1
IgroGames2227 <izhukov2007@gmail.com>
2021-08-04 14:47:34 +0700

    hmm...

--- src/main/java/com/algorithmlx/liaveres/common/registry/ModItems.java
+++ src/main/java/com/algorithmlx/liaveres/common/registry/ModItems.java
@@ -9,12 +9,16 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
     //ITEM
     public static final Item MATTER_CRYSTAL =  new Item(new Item.Settings().group(LiaVeres.LiaVeres_GroupItems));
+    public static final Item MATTER =  new Item(new Item.Settings());
 
     //BlockItems
     public static final BlockItem MATTER_CRYSTAL_BLOCK = new BlockItem(ModBlocks.MATTER_CRYSTAL_BLOCK, new Item.Settings().group(LiaVeres.LiaVeres_GroupBlocks));
 
     public static void registerItems() {
+        //Reg Item
         Registry.register(Registry.ITEM, new Identifier(LiaVeres.ModId, "matter_crytal"), MATTER_CRYSTAL);
+        Registry.register(Registry.ITEM, new Identifier(LiaVeres.ModId, "matter"), MATTER);
+        //Reg Block
         Registry.register(Registry.ITEM, new Identifier(LiaVeres.ModId, "matter_crystal_block"), MATTER_CRYSTAL_BLOCK);
     }
 }
